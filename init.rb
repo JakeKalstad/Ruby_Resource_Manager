f891034ea7a4b1f2afc06ebedabc2d81e9cdcb79
Gnostus <JakeKalstad@yahoo.com>
2010-07-17 22:48:53 -0700

    Removed the debug lines from the start up (loading all the data from the database gets nasty when 5 or more, 100 plus lined resource files are stored.

--- init.rb
+++ init.rb
@@ -3,10 +3,10 @@ require File.dirname(__FILE__) + "/setup_db"
 
 Setup_DB.call_forth_the_beast if not File.exist? File.dirname(__FILE__) + "/resource.db"
  @data_base = SQLite3::Database.new( "resource.db" )
-
-p '~~~~~~~~~~~~~~DB OUTPUT ~~~~~~~~~~~~~~~~~~~~~'
-p @data_base.execute("select * from save")
-p @data_base.execute("select * from resource_pairs")
-p '~~~~~~~~~~~~~~DB OUTPUT ~~~~~~~~~~~~~~~~~~~~~'
+#uncomment for debugging data
+#p '~~~~~~~~~~~~~~DB OUTPUT ~~~~~~~~~~~~~~~~~~~~~'
+#p @data_base.execute("select * from save")
+#p @data_base.execute("select * from resource_pairs")
+#p '~~~~~~~~~~~~~~DB OUTPUT ~~~~~~~~~~~~~~~~~~~~~'
 
 MainForm.new.main_loop
